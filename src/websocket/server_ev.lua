588fd502c9d67cf175cf0d690e04e3bffd1e5f8a
Stimor <kleska16@gmail.com>
2025-09-29 22:19:43 +0300

    Update server_ev.lua

--- src/websocket/server_ev.lua
+++ src/websocket/server_ev.lua
@@ -1,6 +1,5 @@
 
 local socket = require'socket'
-local tools = require'websocket.tools'
 local frame = require'websocket.frame'
 local handshake = require'websocket.handshake'
 local tconcat = table.concat
